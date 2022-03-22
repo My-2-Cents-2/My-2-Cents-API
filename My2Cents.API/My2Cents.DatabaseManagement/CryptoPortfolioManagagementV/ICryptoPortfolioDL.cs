4893a2a45d15bd9a52920139888d9609584db3a2
vijhan <97991833+vijhan@users.noreply.github.com>
2022-03-22 16:01:54 -0700

    start datalayer

---
+++ My2Cents.API/My2Cents.DatabaseManagement/CryptoPortfolioManagagementV/ICryptoPortfolioDL.cs
@@ -0,0 +1,9 @@
+using My2Cents.DataInfrastructure;
+
+namespace My2Cents.DatabaseManagement.Interfaces
+{
+    public interface ICryptoPortfolioDL
+    {
+        Crypto AddCrypto(Crypto )
+    }
+}
