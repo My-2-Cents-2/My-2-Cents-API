8f4edf68984e17fceab3ed994a19e5bba6d62023
Brian Dawkins <brian.dawkins@reature.net>
2022-03-30 15:17:20 -0400

    Buy and Selling of Crypto
    Co-authored-by: Kira <kiranguyen224@gmail.com>

---
+++ My2Cents.API/My2Cents.DatabaseManagement/InvestmentPlatformManagement/SupportedModels/MarketOhlcv.cs
@@ -0,0 +1,38 @@
+using Newtonsoft.Json;
+
+namespace My2Cents.DatabaseManagement.Models
+{
+    public class MarketDataOhlcv
+    {
+        [JsonProperty("market_cap_rank")]
+        public long? MarketCapRank { get; set; }
+
+        [JsonProperty("price_change_24h")]
+        public decimal? PriceChange24H { get; set; }
+
+        [JsonProperty("price_change_percentage_24h")]
+        public double? PriceChangePercentage24H { get; set; }
+
+        [JsonProperty("market_cap_change_24h")]
+        public decimal? MarketCapChange24H { get; set; }
+
+        [JsonProperty("market_cap_change_percentage_24h")]
+        public decimal? MarketCapChangePercentage24H { get; set; }
+
+        [JsonProperty("circulating_supply")]
+        public string CirculatingSupply { get; set; }
+
+        [JsonProperty("total_supply")]
+        public decimal? TotalSupply { get; set; }
+        
+    }
+
+    public class Roi
+    {
+        [JsonProperty("times")] public decimal? Times { get; set; }
+
+        [JsonProperty("currency")] public string Currency { get; set; }
+
+        [JsonProperty("percentage")] public decimal? Percentage { get; set; }
+    }
+}
